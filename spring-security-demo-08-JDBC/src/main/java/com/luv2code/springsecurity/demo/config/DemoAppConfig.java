8aaeb4020024219eea1425ea046b4a5f5ca6a96c
Rahul Ratra <rahul.ratra51@gmail.com>
2020-05-19 17:11:36 -0400

    <Commit>

--- spring-security-demo-08-JDBC/src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
+++ spring-security-demo-08-JDBC/src/main/java/com/luv2code/springsecurity/demo/config/DemoAppConfig.java
@@ -3,7 +3,10 @@ package com.luv2code.springsecurity.demo.config;
 
 
 
+import java.beans.PropertyVetoException;
 import java.util.logging.Logger;
+
+import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +17,8 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 @Configuration
 @EnableWebMvc
@@ -36,5 +41,45 @@ public class DemoAppConfig
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
 	}
+	
+	@Bean
+	public DataSource securityDataSource()
+	{
+		
+		ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+		
+		try {
+			securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+		} catch (PropertyVetoException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		logger.info(">>>> jdbc.url="+env.getProperty("jdbc.url"));
+		logger.info(">>>> jdbc.user="+env.getProperty("jdbc.user"));
+		
+		securityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+		securityDataSource.setUser(env.getProperty("jdbc.user"));
+		securityDataSource.setPassword(env.getProperty("jdbc.password"));
+		
+		
+		
+		securityDataSource.setInitialPoolSize(getInproperty("connection.pool.initialPoolSize"));
+		securityDataSource.setMinPoolSize(getInproperty("connection.pool.minPoolSize"));
+		securityDataSource.setMaxPoolSize(getInproperty("connection.pool.maxPoolSize"));
+		securityDataSource.setMaxIdleTime(getInproperty("connection.pool.maxIdleTime"));
+		
+		return securityDataSource();
+		
+		
+	}
+	private int getInproperty(String propName)
+	{
+		String propVal= env.getProperty(propName);
+		
+		int intpropVal= Integer.parseInt(propVal);
+		return intpropVal;
+	}
+	
 	
 }
